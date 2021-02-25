65c7c93263b39f1f59f36804c1ba2d58041ed3f8
Wes Cravens <wcravens@cortex-it.com>
2021-02-25 08:05:10 -0600

    Move most things from main() to support functions.

--- src/main.cpp
+++ src/main.cpp
@@ -11,43 +11,27 @@ void abort( const std::string message );
 void quit();
 void quit( const std::string message );
 
+void initialize_window( GLFWwindow* w);
+void initialize_glew();
+void log_gl_version_info();
+void terminate_window();
+
 const int WINDOW_HEIGHT=1600;
 const int WINDOW_WIDTH=1200;
 const char* WINDOW_TITLE="Antons OpenGL Tutorial";
 
-GLFWwindow* window;
 int main() {
   if( !glfwInit() ) abort( "Could not start GLFW3" );
 
-#ifdef __APPLE__
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#endif
-
-  window = glfwCreateWindow( WINDOW_HEIGHT, WINDOW_WIDTH, WINDOW_TITLE, NULL, NULL );
-  if( !window ) {
-    glfwTerminate();
-    abort( "Could not open window with GLFW3" );
-  }
-  glfwMakeContextCurrent( window );
-
-  glewExperimental = GL_TRUE;
-  glewInit();
-  if (glewInit() != GLEW_OK)
-      abort( "Failed to initialize GLEW");
-
-  const std::string renderer( reinterpret_cast< char const* >( glGetString( GL_RENDERER ) ) );
-  const std::string version( reinterpret_cast< char const* >( glGetString( GL_VERSION ) ) );
-  log( "Renderer: " + renderer );
-  log( "OpenGL Version support: " + version );
+  GLFWwindow* window;
+  initialize_window( window );
+  initialize_glew();
+  log_gl_version_info();
 
   glEnable( GL_DEPTH_TEST );
   glDepthFunc( GL_LESS );
 
-  glfwTerminate();
-  quit();
+  terminate_window();
 
 }
 
@@ -74,3 +58,38 @@ void quit(){
 void quit( const std::string message ){
   log( "QUIT::" + message );
 }
+
+void initialize_glew(){
+  glewExperimental = GL_TRUE;
+  glewInit();
+  if (glewInit() != GLEW_OK)
+      abort( "Failed to initialize GLEW");
+}
+
+void log_gl_version_info(){
+  const std::string version( reinterpret_cast< char const* >( glGetString( GL_VERSION ) ) );
+  log( "Renderer: " + std::string( reinterpret_cast< char const* >( glGetString( GL_RENDERER ) ) ) );
+  log( "OpenGL Version support: " + version );
+}
+
+void initialize_window( GLFWwindow* w ){
+  #ifdef __APPLE__
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  #endif
+
+  w = glfwCreateWindow( WINDOW_HEIGHT, WINDOW_WIDTH, WINDOW_TITLE, NULL, NULL );
+  if( !w ) {
+    glfwTerminate();
+    abort( "Could not open window with GLFW3" );
+  }
+  glfwMakeContextCurrent( w );
+}
+
+void terminate_window(){
+  glfwTerminate();
+  quit();
+}
+
